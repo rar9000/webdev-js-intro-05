784c3897c0c5313c25bea910d352d7078c7b8113
rar9000 <clavius@Clavius.local>
2025-01-10 19:09:48 -0500

    first part done

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+/*
+
+let computer_Sandwhich = [1, "Turkey", true, "Sriracha", 2 < 1, "Pepperjack", 2];
+
+// console.log(computer_Sandwhich); // Output: [ 1, 'Turkey', true, 'Sriracha', false, 'Pepperjack', 2 ]
+
+// console.log(typeof computer_Sandwhich); // Output: object
+
+computer_Sandwhich.shift();    // Removing the first element with the shift method
+console.log(computer_Sandwhich); // Output [ 'Turkey', true, 'Sriracha', false, 'Pepperjack', 2 ]
+
+console.log(computer_Sandwhich[3]);    // Output: false
+
+computer_Sandwhich.push(9);    // Add an element to the end of our array with the push method
+console.log(computer_Sandwhich);   // Output: [ 'Turkey', true, 'Sriracha', false, 'Pepperjack', 2, 9 ]
+
+*/
